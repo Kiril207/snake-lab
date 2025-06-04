977fd28eaf919b3c947cff2348509896ec1e7a5f
Kiril207 <213026559+Kiril207@users.noreply.github.com>
2025-06-04 18:53:30 +0300

    add

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/maks1/Desktop/лр6/snake-game/04 Snake/main.cpp" "CMakeFiles/SnakeGame.dir/04_Snake/main.cpp.obj" "gcc" "CMakeFiles/SnakeGame.dir/04_Snake/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
